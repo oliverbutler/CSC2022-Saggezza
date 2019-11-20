b231687127f73eea0df5953cc59af0e88b08d243
Oliver Butler <oliver@oliverbutler.uk>
2019-11-20 13:32:21 +0000

    Update server

--- server/app.py
+++ server/app.py
@@ -1,10 +1,79 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+data = [
+  {
+    'id': '1',
+    'name': 'Trip to Sunderland',
+    'date': '12/11/2019',
+    'status': 'pending',
 
-@app.route('/')
+  },
+  {
+    'id': '2',
+    'name': 'Trip to London',
+    'date': '09/11/2019',
+    'status': 'approved',
+  },
+  {
+    'id': '3',
+    'name': 'Trip to Manchester',
+    'date': '09/11/2019',
+    'status': 'denied',
+  },
+  {
+    'id': '4',
+    'name': 'Trip to Sunderland',
+    'date': '12/11/2019',
+    'status': 'pending',
+  },
+  {
+    'id': '5',
+    'name': 'Trip to London',
+    'date': '09/11/2019',
+    'status': 'approved',
+  },
+  {
+    'id': '6',
+    'name': 'Trip to Manchester',
+    'date': '09/11/2019',
+    'status': 'denied',
+  },
+  {
+    'id': '7',
+    'name': 'Trip to London',
+    'date':  '09/11/2019',
+    'status': 'approved',
+  },
+  {
+    'id': '8 ',
+    'name': 'Trip to Manchester',
+    'date': '09/11/2019',
+    'status': 'denied',
+  },
+  {
+    'id': '9',
+    'name': 'Trip to Sunderland',
+    'date': '12/11/2019',
+    'status': 'pending',
+  },
+  {
+    'id': '10',
+    'name': 'Trip to London',
+    'date': '09/11/2019',
+    'status': 'approved',
+  },
+  {
+    'id': '11',
+    'name': 'Trip to Manchester',
+    'date': '09/11/2019',
+    'status': 'denied',
+  },
+]
+
+@app.route('/requests')
 def hello():
-  return "Hello World!"
+  return jsonify(data)
 
 if __name__ == '__main__':
   app.run()
