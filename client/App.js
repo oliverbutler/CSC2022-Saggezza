d533a02fe0b881fe4081b387d1615c73f728522e
Oliver Butler <oliver@oliverbutler.uk>
2020-01-30 17:38:08 +0000

    Basic navigation layout

---
+++ client/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+//Pull in AppNavigation from the navigation folder
+import AppNavigation from "./navigation/AppNavigation";
+
+export default function App() {
+  return <AppNavigation />;
+}
