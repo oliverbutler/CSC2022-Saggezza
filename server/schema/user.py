4568628687c75599b9c5dd8e4260619fc3ccafe5
Oliver Butler <oliver@oliverbutler.uk>
2020-02-10 02:31:42 +0000

    Start Marshmallow User Schema

---
+++ server/schema/user.py
@@ -0,0 +1,10 @@
+from marshmallow import Schema, fields, validates, ValidationError
+from marshmallow.validate import Length
+from functions import *
+
+
+class UserListSchema(Schema):
+    email = fields.Email(required=True, validate=Length(max=32))
+    first_name = fields.Str(required=True, validate=Length(max=32))
+    last_name = fields.Str(required=True, validate=Length(max=32))
+    role = fields.Str(required=True)
