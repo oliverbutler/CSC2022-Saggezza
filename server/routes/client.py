f8ab14f666953f801cc295d6d9d331557d345a82
Jenny Dwyer-Joyce <j.dwyerjoyce@gmail.com>
2020-02-11 13:18:17 +0000

    Added Client and ClientList API

--- server/routes/client.py
+++ server/routes/client.py
@@ -3,8 +3,9 @@ from flask_restful import Resource
 from functions import *
 from mongoengine import *
 from schema.category import *
+from schema.client import *
 
-from model import User
+from model import Client
 
 
 # Connect to mongodb
@@ -17,10 +18,22 @@ class ClientListAPI(Resource):
     # |- GET: Return list of clients
 
     def post(self):
+        req = parse(request)
+        errors = ClientListSchema().validate(req)
+        if errors:
+            return res('Errors in request', 'alert', errors=errors), 400
+
+        client = Client(
+            name = req['name'],
+            email = req['email'],
+        )
+        client.save()
+
         return res('Added a new client', 'success')
 
     def get(self):
-        return res('Returned list of clients', 'success')
+        clients = Client.objects().all()
+        return res('Returned list of clients', 'success', clients = convert_query(clients))
 
 
 class ClientAPI(Resource):
@@ -30,10 +43,34 @@ class ClientAPI(Resource):
     # |- GET: Return client
 
     def put(self, id):
-        return res('Modified client', 'success')
+        req = parse(request)
+        errors = ClientSchema().validate(req)
+        if errors:
+            return res('Errors in request', 'alert', errors=errors), 400
+        try:
+            client = Client.objects(id=id)[0]
+        except:
+            return res("Client doesn't exist", 'error'), 400
+
+        for i in req:
+            client[i] = req[i]
+
+        return res('Modified client', 'success', client=convert_query(client))
+
 
     def delete(self, id):
+        try:
+            client = Client.objects(id=id)
+        except:
+            return res("Client doesn't exist", 'error'), 400
+
+        client.delete()
+
         return res('Deleted client', 'success')
 
     def get(self, id):
-        return res('Returned client', 'success')
+        try:
+            client = Client.objects(id=id)[0]
+            return res('Returned client', 'success', client=convert_query(client))
+        except:
+            return res("Client doesn't exist", 'error'), 400
--- server/schema/client.py
+++ server/schema/client.py
@@ -1,3 +1,12 @@
 from marshmallow import Schema, fields, validates, ValidationError
 from marshmallow.validate import Length
 from functions import *
+
+class ClientListSchema(Schema):
+    name = fields.Str(required=True, validate=Length(max=32))
+    email = fields.Email(required=True, validate=Length(max=32))
+
+
+class ClientSchema(Schema) : 
+    name = fields.Str(required=True, validate=Length(max=32))
+    email = fields.Email(required=True, validate=Length(max=32))
