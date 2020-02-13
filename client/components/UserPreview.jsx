3a19d910c3a17fab849c9470f59e6ae4541b26e8
Oliver Butler <oliver@oliverbutler.uk>
2020-02-13 12:19:54 +0000

    Edit text

--- client/components/UserPreview.jsx
+++ client/components/UserPreview.jsx
@@ -6,7 +6,7 @@ export class UserPreview extends Component {
     return (
       <View style={{ padding: 15 }}>
         <Text>First Name: {this.props.user.first_name}</Text>
-        <Text>Email: {this.props.user.email}</Text>
+        <Text>User Email: {this.props.user.email}</Text>
       </View>
     );
   }
