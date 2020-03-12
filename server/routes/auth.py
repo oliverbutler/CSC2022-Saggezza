852d300b495cfb8dbe269bf359bc11de71c27479
Oliver Butler <oliver@oliverbutler.uk>
2020-03-12 16:21:50 +0000

    :bug: removed debug code
    yeet = 1/ 0

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -26,7 +26,6 @@ auth = HTTPTokenAuth(scheme="Bearer")
 @auth.verify_token
 def verify_token(token):
     if token == "":
-        yeet = 1 / 0
         return False
 
     # Decode token to access the google_id
@@ -34,14 +33,12 @@ def verify_token(token):
     try:
         caller = jwt.decode(token, verify=False)
     except:
-        yeet = 1 / 0
         return False
 
     # Now find the appropriate user
     try:
         user = User.objects.get(id=caller["id"])
     except:
-        yeet = 1 / 0
         return False
 
     # Try verify the token with this users secret
@@ -49,7 +46,6 @@ def verify_token(token):
     try:
         decode = jwt.decode(token.encode("utf-8"), user["secret"])
     except jwt.exceptions.InvalidSignatureError:
-        yeet = 1 / 0
         return False
 
     return True
